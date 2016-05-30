b5bdd4559cbb790a37a7f89e31fe07e93a3de9cd
Maxim Burak <makc.burak@gmail.com>
2016-05-30 22:58:15 +0300

    MyFirstCommit

---
+++ localizations.js
@@ -0,0 +1,10 @@
+String.toLocaleString({
+	"en": {
+		"%title": "English ",
+		"%info": "You are viewing an English localization of this page."
+	},
+	"ru": {
+		"%title": "Русский ",
+		"%info": "Вы просматриваете Русскую версию этой страницы."
+	}
+});
